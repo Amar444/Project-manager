ee030b468a5a513c1e2a385c35bef4b73ee778c1
amar09 <tursicamar@gmail.com>
2014-03-20 09:29:09 +0100

    first initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meesterproefconcept::Application.config.secret_key_base = '197adbfb2df32d5187bd9903d92fb71e769aa79f85da46c17032992863edb120775b2664bdc55df2e1ae6d758eb88898c359b13e99106f6947f707c103345d6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Meesterproefconcept::Application.config.session_store :cookie_store, key: '_meesterproefconcept_session'
